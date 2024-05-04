821f914cec7b36d2763668c9b5cda56c3c4e8ba0
Ribár Krisztián <rybarkristik@gmail.com>
2024-05-04 23:10:53 +0200

    progressed further in the course.

---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/cruddemo/src/test/java/bluu/cat/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package bluu.cat.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
