32ffa26ce4b81ef674bfffd433ac42e9c4c9984b
Ribár Krisztián <rybarkristik@gmail.com>
2024-07-08 23:39:39 +0200

    Progressed further in the course, looking into Rest Controllers

---
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/test/java/com/bluu/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
