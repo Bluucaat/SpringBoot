27662ef079790eb2829e537ee12c6af32dc95e3e
Ribár Krisztián <rybarkristik@gmail.com>
2024-07-29 23:16:15 +0200

    Progressed further in the course, covering REST configs and Sorting

--- 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/bluu/springboot/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/bluu/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,6 +2,9 @@ package com.bluu.springboot.cruddemo.dao;
 
 import com.bluu.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+@RepositoryRestResource(path="memebers")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
