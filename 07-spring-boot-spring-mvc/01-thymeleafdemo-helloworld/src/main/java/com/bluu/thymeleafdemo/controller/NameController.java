685de1be99efe04bb09037c08b89a76cfa8b93d7
Ribar Krisztian <rybarkristik@gmail.com>
2024-09-18 22:33:49 +0900

    Progressed further, making a form with MVC and thymeleaf

---
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/bluu/thymeleafdemo/controller/NameController.java
@@ -0,0 +1,19 @@
+package com.bluu.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NameController {
+
+    @GetMapping("/form")
+    public String sendForm(){
+        return "form";
+    }
+
+    @GetMapping("/processForm")
+    public String processForm(){
+        return "datedemo";
+    }
+
+}
