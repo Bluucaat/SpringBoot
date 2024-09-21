ee7acc06b6dc0d3d4d330f6d5120b1be6908d7ad
Ribar Krisztian <rybarkristik@gmail.com>
2024-09-21 23:43:04 +0900

    reviewed getMapping/postMapping, looked at another way to request parameter(s)

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/bluu/thymeleafdemo/controller/DemoController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/bluu/thymeleafdemo/controller/DemoController.java
@@ -1,15 +1,34 @@
 package com.bluu.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DemoController {
 
     @GetMapping("/date")
-    public String sayHello(Model model) {
+    public String greetUser(Model model) {
         model.addAttribute("date", java.time.LocalDate.now());
+        return "datedemo";
+    }
+
+    @RequestMapping("/processForm2")
+    public String greetUserAndStealHisData(HttpServletRequest request, Model model){
+
+        String greeting = "Hello, " + request.getParameter("name").toUpperCase();
+        model.addAttribute("greeting", greeting);
+
+        return "datedemo";
+    }
+
+    @RequestMapping("/processForm3")
+    public String greetUserAndStealHisDataMoreAdvanced(@RequestParam("name") String name, Model model){
+
+        model.addAttribute("greeting", name.toUpperCase());
         return "datedemo";
     }
 }
