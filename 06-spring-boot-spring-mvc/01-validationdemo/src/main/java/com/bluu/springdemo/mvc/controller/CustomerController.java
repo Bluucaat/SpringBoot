10844b8c62bfb3b6936a18492b0af959e982aab5
Ribár Krisztián <rybarkristik@gmail.com>
2024-09-26 08:50:36 +0900

    covered mvc validation with a basic name form

--- 06-spring-boot-spring-mvc/01-validationdemo/src/main/java/com/bluu/springdemo/mvc/controller/CustomerController.java
+++ 06-spring-boot-spring-mvc/01-validationdemo/src/main/java/com/bluu/springdemo/mvc/controller/CustomerController.java
@@ -2,9 +2,13 @@ package com.bluu.springdemo.mvc.controller;
 
 
 import com.bluu.springdemo.mvc.model.Customer;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
@@ -12,5 +16,12 @@ public class CustomerController {
     public String showForm(Model theModel) {
         theModel.addAttribute("customer", new Customer());
         return "customer-form";
+    }
+    @PostMapping("/processForm")
+    public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult result) {
+        if(result.hasErrors()) {
+            return "customer-form";
+        }
+        return "form-confirmation";
     }
 }
--- 06-spring-boot-spring-mvc/01-validationdemo/src/main/java/com/bluu/springdemo/mvc/model/Customer.java
+++ 06-spring-boot-spring-mvc/01-validationdemo/src/main/java/com/bluu/springdemo/mvc/model/Customer.java
@@ -3,12 +3,12 @@ package com.bluu.springdemo.mvc.model;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 public class Customer {
-    @NotNull(message="required")
-    @Size(min=1, message="required")
-    private String firstName = "";
-    @NotNull(message="required")
-    @Size(min=1, message="required")
-    private String lastName = "";
+    @NotNull(message="required field")
+    @Size(min=1, message="required field")
+    private String firstName;
+    @NotNull(message="required field")
+    @Size(min=1, message="required field")
+    private String lastName;
 
 
     public String getFirstName() {
