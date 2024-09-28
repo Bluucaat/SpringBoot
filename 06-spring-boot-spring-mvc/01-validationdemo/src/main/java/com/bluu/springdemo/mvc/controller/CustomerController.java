175588ef2916b631abd6bc3c27640fa3e5eb860c
Ribár Krisztián <rybarkristik@gmail.com>
2024-09-28 13:50:13 +0900

    Introduced the use of initBinder to handle null/untrimmed strings passed to the form

--- 06-spring-boot-spring-mvc/01-validationdemo/src/main/java/com/bluu/springdemo/mvc/controller/CustomerController.java
+++ 06-spring-boot-spring-mvc/01-validationdemo/src/main/java/com/bluu/springdemo/mvc/controller/CustomerController.java
@@ -3,15 +3,26 @@ package com.bluu.springdemo.mvc.controller;
 
 import com.bluu.springdemo.mvc.model.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    //handling whitespaces
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        binder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
     @GetMapping("/form")
     public String showForm(Model theModel) {
         theModel.addAttribute("customer", new Customer());
@@ -19,6 +30,7 @@ public class CustomerController {
     }
     @PostMapping("/processForm")
     public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult result) {
+        System.out.println("Last Name: |" + theCustomer.getLastName() + "|");
         if(result.hasErrors()) {
             return "customer-form";
         }
